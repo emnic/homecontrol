ac3e85d97a6c44c105aaef6bc31f6f053e44a946
Tester <emnic>
2015-08-02 21:24:08 +0200

    Added functionality to save device on server

--- public/javascripts/app/controllers/devicesController.js
+++ public/javascripts/app/controllers/devicesController.js
@@ -5,11 +5,24 @@
         .module('homeControl')
         .controller('devicesController', devicesController)
         .factory('devices', ['$http', function($http){
-            var factory = {};
-            factory.getAll = function() {
+            var o = {
+              devices: []
+            };
+
+            o.getAll = function() {
                 return $http.get('/devices');
             };
-            return factory;
+
+            o.create = function(post) {
+              return $http.post('/devices', post).success(function(data){
+                o.devices.push(data);
+              });
+            };
+            o.delete = function(deviceId) {
+              return $http.delete('/devices', {id: deviceId});
+            };
+
+            return o;
         }]);
 
     devicesController.$inject = ['resolvedDevices', 'devices', 'resolvedTimers'];
@@ -24,16 +37,20 @@
 
         vm.addDevice = function () {
           var num_devices = vm.list_of_devices.length + 1
-          vm.list_of_devices.push(
-          { name: "NoName Device " + num_devices,
-            timers:null
-          })
+          var device = {name: "NoName Device " + num_devices,
+                        timers:null
+                        };
+          vm.list_of_devices.push(device);
+
+          devices.create(device);
         };
 
         vm.removeDevice = function (device) {
           var num_devices = vm.list_of_devices.length - 1
           var index = vm.list_of_devices.indexOf(device);
+          console.log(device);
           vm.list_of_devices.splice(index, 1);
+          devices.delete(device._id);
         };
 
         vm.saveDevice = function () {
