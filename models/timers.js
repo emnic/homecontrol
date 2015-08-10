8834da67611d1b499417a3f0b0f57ab863dba2b0
emnic <emilnicklasson@hotmail.com>
2015-08-10 15:55:36 +0200

    Tmp commit for update on timers

--- models/timers.js
+++ models/timers.js
@@ -18,10 +18,16 @@ var TimersSchema = new mongoose.Schema({
              }]
 });
 
-TimersSchema.methods.updateOnTime = function(updateTime, cb) {
+TimersSchema.methods.updateOnTime = function(updateTime, callback) {
 
   this.schedules[0].on_time = updateTime.on_time;
-  this.save(cb);
+  this.save(callback);
+};
+
+TimersSchema.methods.addSchedule = function(newSchedule, callback) {
+  console.log(newSchedule);
+  this.schedules.push(newSchedule.schedule);
+  this.save(callback);
 };
 
 module.exports = mongoose.model('timers', TimersSchema);
--- public/javascripts/app/controllers/timersController.js
+++ public/javascripts/app/controllers/timersController.js
@@ -5,11 +5,39 @@
         .module('homeControl')
         .controller('timersController', timersController)
         .factory('timers', ['$http', function($http){
-            var factory = {};
-            factory.getAll = function() {
-                return $http.get('/timers');
+            var o = {
+              timers: []
             };
-            return factory;
+
+            o.getAll = function() {
+                return $http.get('/timers').success(function(data){
+                  o.timers = angular.copy(data);
+                });
+            };
+
+            o.create = function(post) {
+              return $http.post('/timers', post).success(function(data){
+                o.timers.push(data);
+              });
+            };
+
+            o.addSchedule = function(timer, schedule) {
+              return $http.put('/timers/' + timer._id + '/add_schedule', schedule).success(function(data){
+                var index = o.timers.indexOf(timer._id);
+                console.log(index);
+                console.log(o.timers);
+                //o.timers[index].schedules.push(data);
+              });
+            };
+          
+            o.deleteTimer = function(timer) {
+              return $http.delete('/timers/' + timer._id).success(function(data){
+                var index = o.timers.indexOf(timer);
+                o.timers.splice(index, 1);
+              });
+            };
+
+            return o;
         }]);
 
 
@@ -28,36 +56,43 @@
 
       vm.addTimer = function () {
         var num_timers = vm.list_of_timers.length + 1
-        vm.list_of_timers.push(
-        { name: "NoName Timer " + num_timers,
+        
+        var timer = {
+          name: "NoName Timer " + num_timers,
           schedules:[]
-        })
-        console.log('Timer Added');
+        };
+        
+        timers.create(timer);
+        vm.list_of_timers = timers.timers;
       };
 
       vm.removeTimer = function (timer) {
           var num_timers = vm.list_of_timers.length - 1
-          var index = vm.list_of_timers.indexOf(timer);
-          vm.list_of_timers.splice(index, 1);
+          
+          timers.deleteTimer(timer);
+          vm.list_of_timers = timers.timers;
       };
 
-      vm.addSchedule = function (schedules) {
-        var num_schedules = schedules.length + 1;
-        schedules.push(
-          { name: "NoName Schedule " + num_schedules,
-            on_time: "12:00",
-            on_variation: "10",
-            off_time: "12:00",
-            off_variation: "20",
-            days: [ {name: "Mon", value: "false"},
-                    {name: "Tue", value: "false"},
-                    {name: "Wed", value: "false"},
-                    {name: "Thu", value: "false"},
-                    {name: "Fri", value: "false"},
-                    {name: "Sat", value: "false"},
-                    {name: "Sun", value: "false"}
-                  ]
-          })
+      vm.addSchedule = function (timer) {
+        var num_schedules = timer.schedules.length + 1;
+        var schedules = {
+          name: "NoName Schedule " + num_schedules,
+          on_time: "12:00",
+          on_variation: "10",
+          off_time: "12:00",
+          off_variation: "20",
+          days: [ {name: "Mon", value: "false"},
+                  {name: "Tue", value: "false"},
+                  {name: "Wed", value: "false"},
+                  {name: "Thu", value: "false"},
+                  {name: "Fri", value: "false"},
+                  {name: "Sat", value: "false"},
+                  {name: "Sun", value: "false"}
+                ]
+        };
+        
+        timers.addSchedule(timer,schedules);
+
       };
 
       vm.checkAll = function (days) {
