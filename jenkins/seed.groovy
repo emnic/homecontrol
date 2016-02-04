186a57e7eca9bc8f8c536501d863754b349b98b8
Tester <emnic>
2016-02-04 21:27:37 +0100

    Moved Jenkins configuration into project

---
+++ jenkins/seed.groovy
@@ -0,0 +1,26 @@
+import jenkins.model.*;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import hudson.tasks.Shell;
+import javaposse.jobdsl.plugin.*;
+
+def url = "https://github.com/emnic/homecontrol.git"
+def jobName = "seed"
+
+project = Jenkins.instance.createProject(FreeStyleProject, jobName)
+def gitScm = new GitSCM(url)
+gitScm.branches = [new hudson.plugins.git.BranchSpec("*/master")]
+project.scm = gitScm
+
+project.getBuildersList().clear()
+
+project.getBuildersList().add(new ExecuteDslScripts(
+  new ExecuteDslScripts.ScriptLocation("false","dsl/**/*.groovy",null),
+  false,
+  RemovedJobAction.IGNORE,
+  RemovedViewAction.IGNORE,
+  LookupStrategy.JENKINS_ROOT,
+  "src/main/groovy")
+);
+
+project.save()
