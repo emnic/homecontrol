c9ad3833a73e9be57f1a9b1b557487c8f0af4a5e
Tester <emnic>
2016-02-11 16:31:36 +0100

    Fixed error in job script

--- dsl/pipeline/gui_test.groovy
+++ dsl/pipeline/gui_test.groovy
@@ -9,9 +9,7 @@ job(jobName) {
         upstream('emnic-homecontrol-master', 'SUCCESS')
     }
     steps {
-        shell("cd app && docker-compose up -d\
-              APP_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' app_web_1)")
-        shell("cd test && docker build -t testrunner \
-             docker run -e APP_IP=$APP_IP -rm testrunner")
+        shell("cd app && docker-compose up -d && APP_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' app_web_1)")
+        shell("cd test && docker build -t testrunner && docker run -e APP_IP=$APP_IP -rm testrunner")
     }
 }
